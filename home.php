ae6ef6ef0c12cb1daad511b7170a7773d056c4f6
github inoyu315 <yuto.41oniku@gmail.com>
2020-06-08 22:13:40 +0900

    home.php stylesheet.cssの編集

---
+++ home.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="stylesheet.css"/>
+    <title>FaBon</title>
+</head>
+<body>
+    <div class="header">
+        <h1>FaBon!</h1>
+        <div class="header-list">
+        <ul>
+            <li>ログイン</li>
+            <li>会員登録</li>
+        </ul>
+        </div>
+    </div>
+    <div class="main">
+        <p>FaBonはあなたのfavoriteな本を紹介できるサイトです。</p>
+        <img src="photos/screen.jpg" alt="本とクッキー">
+    </div>
+</body>
+</html>
